1f9564ba1e32611bf8a816cd9165e334dbc318f6
tusimbe <tusimbe@gmail.com>
2014-08-21 18:13:07 +0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,13 @@ RSpec.describe User, :type => :model do
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:remember_token) }
 	it { should be_valid }
+
+	describe "remember token" do
+		before { @user.save }
+		it { expect(@user.remember_token).not_to be_blank }
+	end
 
 	describe "when name is not present" do
 		before { @user.name = "" }
